a94c7de034aa779d01757e324b3bef5de317aa28
Tom Opdebeeck <tom230691@gmail.com>
2017-07-25 17:09:34 +0200

    update mappings
    * add version contenttype
    * linting

--- app/config/mappings/index.js
+++ app/config/mappings/index.js
@@ -1,11 +1,10 @@
-var customItems = require("./customItems");
 var product = require("./product");
 var version = require("./version");
 
 module.exports = {
-    index: "local_content",
-    mappings: {
-        product: product,
-        version: version
-    }
+	index: "acpaas_content",
+	mappings: {
+		product: product,
+		version: version,
+	},
 };
--- app/config/mappings/product.js
+++ app/config/mappings/product.js
@@ -3,98 +3,98 @@
 var customItems = require("./customItems");
 
 module.exports = {
-    properties: {
-        fields: {
-            type: "nested",
-            properties: {
-                title: {
-                    type: "string"
-                },
-                intro: {
-                    type: "string"
-                },
-                about: {
-                    type: "string"
-                },
-                roadmap: {
-                    type: "nested",
-                    properties: {
-                        fields: {
-                            type: "object",
-                            properties: {
-                                title: {
-                                    type: "string"
-                                },
-                                notes: {
-                                    type: "string"
-                                },
-                                version: {
-                                    type: "string"
-                                }
-                            }
-                        },
-                        uuid: {
-                            index: "no",
-                            type: "string"
-                        }
-                    }
-                },
-                customItems: customItems
-            }
-        },
-        meta: {
-            type: "object",
-            properties: {
-                activeLanguages: {
-                    index: "no",
-                    type: "string"
-                },
-                contentType: {
-                    index: "no",
-                    type: "string"
-                },
-                created: {
-                    index: "no",
-                    type: "date",
-                    format: "strict_date_optional_time||epoch_millis"
-                },
-                lastModified: {
-                    index: "no",
-                    type: "date",
-                    format: "strict_date_optional_time||epoch_millis"
-                },
-                publishDate: {
-                    index: "no",
-                    type: "date",
-                    format: "strict_date_optional_time||epoch_millis"
-                },
-                slug: {
-                    index: "no",
-                    type: "string"
-                },
-                taxonomy: {
-                    type: "object",
-                    properties: {
-                        tags: {
-                            type: "nested",
-                            properties: {
-                                id: {
-                                    index: "no",
-                                    type: "string"
-                                },
-                                label: {
-                                    index: "no",
-                                    type: "string"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        uuid: {
-            index: "no",
-            type: "string"
-        }
-    }
+	properties: {
+		fields: {
+			type: "nested",
+			properties: {
+				title: {
+					type: "string",
+				},
+				intro: {
+					type: "string",
+				},
+				about: {
+					type: "string",
+				},
+				roadmap: {
+					type: "nested",
+					properties: {
+						fields: {
+							type: "object",
+							properties: {
+								title: {
+									type: "string",
+								},
+								notes: {
+									type: "string",
+								},
+								version: {
+									type: "string",
+								},
+							},
+						},
+						uuid: {
+							index: "no",
+							type: "string",
+						},
+					},
+				},
+				customItems: customItems,
+			},
+		},
+		meta: {
+			type: "object",
+			properties: {
+				activeLanguages: {
+					index: "no",
+					type: "string",
+				},
+				contentType: {
+					index: "no",
+					type: "string",
+				},
+				created: {
+					index: "no",
+					type: "date",
+					format: "strict_date_optional_time||epoch_millis"
+				},
+				lastModified: {
+					index: "no",
+					type: "date",
+					format: "strict_date_optional_time||epoch_millis"
+				},
+				publishDate: {
+					index: "no",
+					type: "date",
+					format: "strict_date_optional_time||epoch_millis"
+				},
+				slug: {
+					index: "no",
+					type: "string",
+				},
+				taxonomy: {
+					type: "object",
+					properties: {
+						tags: {
+							type: "nested",
+							properties: {
+								id: {
+									index: "no",
+									type: "string",
+								},
+								label: {
+									index: "no",
+									type: "string",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		uuid: {
+			index: "no",
+			type: "string",
+		},
+	},
 };
