f8cd0d9930a2125bd62d434f9d6f7ef437490f50
Tom Opdebeeck <tom230691@gmail.com>
2017-07-25 19:02:56 +0200

    add update listeners

--- app/controllers/listeners.js
+++ app/controllers/listeners.js
@@ -1,15 +1,54 @@
 var Emitter = require("app/middleware/emitter");
 
+var contentTypes = require("../helpers.contentTypes").list;
+var productHelper = require("../helpers/product");
+var versionHelper = require("../helpers/version");
+
+var actions = {
+	product: {
+		create: productHelper.syncProduct,
+		update: productHelper.updateProduct,
+		remove: productHelper.removeProduct,
+	},
+	version: {
+		create: versionHelper.syncVersion,
+		update: versionHelper.updateVersion,
+		remove: versionHelper.removeVersion,
+	},
+};
+
+function verifyContentType(type) {
+	type = typeof type === "string" ? type : type._id;
+
+	return contentTypes.find(function(t) {
+		return t.id === type;
+	});
+}
+
+function verifyAction(action, contentType) {
+	return actions.hasOwnProperty(contentType.type) ? actions[contentType.type][action] : function() {};
+}
+
+function handleUpdate(contentItem, action) {
+	var contentType = verifyContentType(contentItem.meta.contentType);
+
+	if (!contentType) {
+		return;
+	}
+
+	return verifyAction(contentType, action)(contentItem);
+}
+
 module.exports.start = function start() {
 	Emitter.on("contentCreated", function(contentItem) {
-		// index new
+		handleUpdate(contentItem, "create");
 	});
 
 	Emitter.on("contentUpdated", function(contentItem) {
-		// index updated
+		handleUpdate(contentItem, "update");
 	});
 
 	Emitter.on("contentRemoved", function(contentItem) {
-		// remove index
+		handleUpdate(contentItem, "remove");
 	});
 };
