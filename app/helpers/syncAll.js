4540b12b0aac13686f01614c3612f1128c764205
Tom Opdebeeck <tom230691@gmail.com>
2017-07-25 19:02:24 +0200

    cleanup syncall
    * use helpers
    * improve readability by chaining
    * remove async in favor of promises

--- app/helpers/syncAll.js
+++ app/helpers/syncAll.js
@@ -3,58 +3,27 @@
 require("rootpath")();
 var Q = require("q");
 var _ = require("lodash");
-var async = require("async");
 
-var ContentModel = require("app/models/content");
-var productId = require("./contentTypes").product;
-var syncProduct = require("./sync").product;
+var productHelper = require("./product");
+var versionHelper = require("./version");
+
+function syncProductsVersions(syncNonModified, products) {
+	return Q.all(products.map(function(product) {
+		return versionHelper
+			.fetchVersions(product)
+			.then(versionHelper.syncVersions.bind(null, syncNonModified))
+			.catch(function(err) {
+				throw err;
+			});
+	}));
+}
 
 module.exports = function(syncNonModified) {
-	var d = Q.defer();
-	var products = [];
-
-	return ContentModel.find({
-		"meta.contentType": productId
-	}, {
-		_id: 0,
-		uuid: 1,
-		fields: 1,
-		"meta.activeLanguages": 1,
-		"meta.contentType": "1",
-		"meta.created": "1",
-		"meta.lastModified": "1",
-		"meta.publishDate": 1,
-		"meta.slug": 1,
-		"meta.taxonomy": 1
-	})
-	.populate("meta.contentType")
-	.lean()
-	.exec()
-	.then(
-		function onSuccess(response) {
-			products = response;
-
-			async.parallel(products.map(function (product) {
-				return function (callback) {
-					syncProduct(product)
-						.then(function(response) {
-							callback(null, response);
-						}, function(err) {
-							callback(err);
-						});
-				};
-			}), function (err, response) {
-				if (err) {
-					return d.reject(err);
-				}
-
-				d.resolve(response);
-			});
-		},
-		function onError(responseError) {
-			throw responseError;
-		}
-	);
-
-	return d.promise;
+	return productHelper
+		.fetchProducts()
+		.then(productHelper.syncProducts.bind(null, syncNonModified))
+		.then(syncProductsVersions.bind(null, syncNonModified))
+		.catch(function(err) {
+			throw err;
+		});
 };
