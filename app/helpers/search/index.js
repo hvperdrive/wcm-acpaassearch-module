1ff8847c8e59e55b443303cbe83226d0d3cd1321
Jeroen Valcke <valcke_jeroen@hotmail.com>
2017-07-26 13:13:59 +0200

    Fix linting errors

--- app/helpers/search/index.js
+++ app/helpers/search/index.js
@@ -3,17 +3,17 @@ var productSearchHelper = require("./product");
 
 module.exports.getQuery = function getQuery(query, type) {
 
-    return {
-        highlight: {
-            order : "score",
-            fields: Object.assign({}, productSearchHelper.getHighlightFields())
-        },
-        query: {
-            bool: {
-                should: [].concat(
-                    productSearchHelper.getQuery(query, type)
-                )
-            }
-        }
-    };
-}
+	return {
+		highlight: {
+			order: "score",
+			fields: Object.assign({}, productSearchHelper.getHighlightFields()),
+		},
+		query: {
+			bool: {
+				should: [].concat(
+					productSearchHelper.getQuery(query, type)
+				),
+			},
+		},
+	};
+};
