7d7be49f015d93cc514aea56e7ad8411cea73285
Tom Opdebeeck <tom230691@gmail.com>
2017-07-27 18:57:35 +0200

    add customItem id

--- app/helpers/contentTypes.js
+++ app/helpers/contentTypes.js
@@ -4,6 +4,7 @@ var contentTypes = {
 	product: "5937cddde77a130b15f572d2", // @todo: remove dummy
 	version: "593eb706aef3a98476c03ba6", // @todo: remove dummy
 	api: "593eb572aef3a98476c03ba1", // @todo: remove dummy
+	customItem: "594d007618b95d24255ea2ec", // @todo: remove dummy
 };
 
 var toList = function(types) {
