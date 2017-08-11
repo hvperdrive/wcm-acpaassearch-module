0c9eb46ec573ff4b7fe34b79c758e33bef50cb5a
Jeroen Valcke <valcke_jeroen@hotmail.com>
2017-08-11 14:03:45 +0200

    bump version

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,18 +1,18 @@
 "use strict";
 
-angular.module("acpaassearch_0.0.36.directives", []);
-angular.module("acpaassearch_0.0.36.factories", []);
-angular.module("acpaassearch_0.0.36.services", ["acpaassearch_0.0.36.factories"]);
-angular.module("acpaassearch_0.0.36.controllers", ["acpaassearch_0.0.36.services"]);
+angular.module("acpaassearch_0.0.37.directives", []);
+angular.module("acpaassearch_0.0.37.factories", []);
+angular.module("acpaassearch_0.0.37.services", ["acpaassearch_0.0.37.factories"]);
+angular.module("acpaassearch_0.0.37.controllers", ["acpaassearch_0.0.37.services"]);
 
 angular
-	.module("acpaassearch_0.0.36", [
+	.module("acpaassearch_0.0.37", [
 		"pelorus.services",
 
-		"acpaassearch_0.0.36.directives",
-		"acpaassearch_0.0.36.factories",
-		"acpaassearch_0.0.36.services",
-		"acpaassearch_0.0.36.controllers",
+		"acpaassearch_0.0.37.directives",
+		"acpaassearch_0.0.37.factories",
+		"acpaassearch_0.0.37.services",
+		"acpaassearch_0.0.37.controllers",
 	])
 	.run([function() {
 		console.log("Members module is available!"); // eslint-disable-line no-console
