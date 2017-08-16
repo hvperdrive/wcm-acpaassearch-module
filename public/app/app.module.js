38f7ec5d1838316082077737b4fb22d77d3562a6
Tom Opdebeeck <tom230691@gmail.com>
2017-08-16 13:53:25 +0200

    version bump

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,18 +1,18 @@
 "use strict";
 
-angular.module("acpaassearch_0.0.37.directives", []);
-angular.module("acpaassearch_0.0.37.factories", []);
-angular.module("acpaassearch_0.0.37.services", ["acpaassearch_0.0.37.factories"]);
-angular.module("acpaassearch_0.0.37.controllers", ["acpaassearch_0.0.37.services"]);
+angular.module("acpaassearch_0.0.38.directives", []);
+angular.module("acpaassearch_0.0.38.factories", []);
+angular.module("acpaassearch_0.0.38.services", ["acpaassearch_0.0.38.factories"]);
+angular.module("acpaassearch_0.0.38.controllers", ["acpaassearch_0.0.38.services"]);
 
 angular
-	.module("acpaassearch_0.0.37", [
+	.module("acpaassearch_0.0.38", [
 		"pelorus.services",
 
-		"acpaassearch_0.0.37.directives",
-		"acpaassearch_0.0.37.factories",
-		"acpaassearch_0.0.37.services",
-		"acpaassearch_0.0.37.controllers",
+		"acpaassearch_0.0.38.directives",
+		"acpaassearch_0.0.38.factories",
+		"acpaassearch_0.0.38.services",
+		"acpaassearch_0.0.38.controllers",
 	])
 	.run([function() {
 		console.log("Members module is available!"); // eslint-disable-line no-console
--- public/app/providers/search-config.provider.js
+++ public/app/providers/search-config.provider.js
@@ -1,12 +1,12 @@
 "use strict";
 
 angular
-	.module("acpaassearch_0.0.37")
+	.module("acpaassearch_0.0.38")
 	.provider("acpaassearchConfig", [
 		function membersConfig() {
 			this.API = {
 				name: "acpaassearch",
-				version: "0.0.37",
+				version: "0.0.38",
 				basePath: "app/modules/",
 			};
 
