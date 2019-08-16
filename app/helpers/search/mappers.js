dc82c675e45c9bec35c1a9f548a9340ece0553bd
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2019-08-16 16:50:37 +0200

    feat: use about instead of intro for search description

--- app/helpers/search/mappers.js
+++ app/helpers/search/mappers.js
@@ -77,7 +77,7 @@ const mapProducts = (products) => {
 			category: _.get(product, "_source.fields.productCategory"),
 			slug: slug,
 			title: _.get(product, "_source.fields.title.value"),
-			description: _.get(product, "_source.fields.intro.value"),
+			description: _.get(product, "_source.fields.about.value") || _.get(product, "_source.fields.intro.value"),
 		};
 	});
 };
